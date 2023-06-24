cae3d92636b19b9942ff3f47e709b42f5acf92b2
DanielBSellers <sel21002@byui.edu>
2023-06-23 23:32:19 -0600

    BoM

---
+++ BoM/scripts/scripts.js
@@ -0,0 +1,25 @@
+// function addChapter() {
+//     const newListItem = document.getElementById("item").value;
+//     console.log(newListItem);
+//     document.getElementById("item").value = '';
+//     const enterListItem = document.createElement("li");
+//     const exSpan = document.createElement("span");
+//     const exBtn = document.createElement("button");
+//     enterListItem.textContent = newListItem;
+//     enterListItem.setAttribute('class', 'liItem')
+//     const theList = document.getElementById("theList");
+//     theList.appendChild(enterListItem);
+//     theList.appendChild(exSpan);
+//     theList.appendChild(exBtn);
+//     // exBtn.setAttribute('id','appndBtn');
+//     exBtn.textContent = '\u274C';
+//     exBtn.addEventListener('click', gonzo());
+//   }
+
+  
+
+//   function gonzo() {
+
+//     console.log('made it this far');
+//     theList.removeChild(enterListItem);
+//   }
