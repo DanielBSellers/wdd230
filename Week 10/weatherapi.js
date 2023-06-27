1c0ec2d52c34f5699bfd5574ee90b1075fee09e7
DanielBSellers <sel21002@byui.edu>
2023-06-26 21:06:58 -0600

    weather api

---
+++ Week 10/weatherapi.js
@@ -0,0 +1,33 @@
+const currentTemp = document.querySelector('#currentTemp');
+const weatherIcon = document.querySelector('#weatherIcon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=6ab4e34bb4e3d065df470068e063f190";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            //console.log(data); // this is for testing the call
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function displayResults(weatherData) {
+    currentTemp.innerHTML = weatherData.main.temp.toFixed(0);
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
+
+apiFetch();
