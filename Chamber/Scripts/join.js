24b361e8c5b6fa9ebe15b2e3feea8ba5cbda7030
DanielBSellers <sel21002@byui.edu>
2023-07-07 08:39:30 -0600

    join and thanks updates

--- Chamber/Scripts/join.js
+++ Chamber/Scripts/join.js
@@ -4,3 +4,8 @@ form.addEventListener("input", function () {
     const jd = new Date();
     document.getElementById('formEdit').innerHTML = jd; 
 });
+
+window.addEventListener("load", function() {
+    const loadingTime = new Date();
+    document.getElementById('hidden').value = loadingTime;
+});
