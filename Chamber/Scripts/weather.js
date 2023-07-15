702117488ffbed1cd4e5dafbebbe8aef278fc221
DanielBSellers <sel21002@byui.edu>
2023-07-15 14:48:31 -0600

    windchill and live weather

--- Chamber/Scripts/weather.js
+++ Chamber/Scripts/weather.js
@@ -1,8 +1,9 @@
-const currentTemp = document.querySelector('#currentTemp');
+const currentTemp = document.querySelector('#temp');
 const weatherIcon = document.querySelector('#weatherIcon');
 const captionDesc = document.querySelector('figcaption');
+const windSpeed = document.querySelector('#wSpeed');
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=6ab4e34bb4e3d065df470068e063f190";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=South Ogden&units=imperial&appid=d276b94d9092d3cec2698fcfd89e5332";
 
 async function apiFetch() {
     try {
@@ -20,6 +21,7 @@ async function apiFetch() {
 
 async function displayResults(weatherData) {
     currentTemp.innerHTML = weatherData.main.temp.toFixed(0);
+    windSpeed.innerHTML = weatherData.wind.speed;
 
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
@@ -30,3 +32,6 @@ async function displayResults(weatherData) {
 }
 
 apiFetch();
+
+// const t = document.getElementById('temp').innerText;
+// const s = document.getElementById('wSpeed').innerText;
--- Chamber/Scripts/windChill.js
+++ Chamber/Scripts/windChill.js
@@ -1,9 +1,21 @@
 const t = document.getElementById('temp').innerText;
-console.log(t);
-const s = document.getElementById('wSpeed').innerText;
-console.log(s);
-const chill = 35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + 0.4275 * (t * (s ** 0.16));
-console.log(chill);
-const wChill = Math.round(chill);
-console.log(wChill);
-document.getElementById('wChill').innerHTML = wChill;
+const speed = document.getElementById('wSpeed').innerText;
+const s = Math.round(speed);
+// const chill = 35.74 + 0.6215*t - 35.75*(s**0.16) + 0.4275*(t*s** 0.16);
+//             // 35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + 0.4275 * (t * (s ** 0.16));            
+//             // 35.74 + 0.6215*temp - 35.75*(speed**0.16) + 0.4275*(temp*speed**0.16);
+// const wChill = Math.round(chill);
+// document.getElementById('wChill').innerHTML = wChill;
+
+function windChill(t, s){
+    if (t <=50 && s >3){
+        let windChill = 35.74 + 0.6215*t - 35.75*(s**0.16) + 0.4275*(t*s**0.16);
+        console.log(windChill);
+        document.getElementById('wChill').innerHTML=Math.round(windChill);
+    }
+    else{
+        document.getElementById('wChill').innerHTML ='N/A';
+    }
+}
+
+windChill();
